13a6c76a341b6ee31388a711be321aea4f25a779
puneetvashisht <puneetvashisht@outlook.com>
2025-11-12 09:29:58 +0530

    basic stuff

---
+++ spring-ai-first-demo/src/main/java/com/pv/spring_ai_first_demo/SpringAiFirstDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pv.spring_ai_first_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiFirstDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiFirstDemoApplication.class, args);
+	}
+
+}
---
+++ spring-ai-first-demo/src/test/java/com/pv/spring_ai_first_demo/SpringAiFirstDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pv.spring_ai_first_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiFirstDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
